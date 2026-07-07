43784a871cdd1576b52755b24950891537b66b52
Wisdom Akpasa <wisdomnemesis@gmail.com>
2026-07-07 13:18:35 +0100

    Changed function name to read better, added function for comparing equality of arrays

--- internal/common/common.go
+++ internal/common/common.go
@@ -8,9 +8,28 @@ func Map[B, C any](bs []B, fn func(B) C) []C {
 	return result
 }
 
-func ZeroNilString(val *string) string {
+func PtrToString(val *string) string {
 	if val != nil {
 		return *val
 	}
 	return ""
+}
+
+func EqualUnordered[T comparable](a, b []T) bool {
+	if len(a) != len(b) {
+		return false
+	}
+
+	counts := make(map[T]int)
+	for _, val := range a {
+		counts[val]++
+	}
+
+	for _, val := range b {
+		if counts[val] == 0 {
+			return false
+		}
+		counts[val]-- // For detecting duplicates
+	}
+	return true
 }
