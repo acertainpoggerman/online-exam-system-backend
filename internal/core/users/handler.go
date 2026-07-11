5045359f7106d656e1e71f91372473ec5fdc9c0b
Wisdom Akpasa <wisdomnemesis@gmail.com>
2026-07-11 19:29:50 +0100

    Changed user service and handler for handling authenticated user routes.

--- internal/core/users/handler.go
+++ internal/core/users/handler.go
@@ -1,106 +1,32 @@
 package users
 
 import (
-	"errors"
 	"net/http"
 
-	"github.com/acertainpoggerman/online-exam-system/internal/apperr"
 	"github.com/acertainpoggerman/online-exam-system/internal/json"
+	"github.com/acertainpoggerman/online-exam-system/internal/jwt"
 )
 
 type UserHandler struct {
-	svc    UserService
-	jwtAge int
+	svc UserService
 }
 
-func NewUserHandler(svc UserService, jwtAge int) *UserHandler {
-	return &UserHandler{svc, jwtAge}
+func NewUserHandler(svc UserService) *UserHandler {
+	return &UserHandler{svc}
 }
 
 func (h *UserHandler) RegisterRoutes(r *http.ServeMux) {
-	r.HandleFunc("POST /register", h.registerUser)
-	r.HandleFunc("POST /login", h.loginUser)
-	r.HandleFunc("POST /logout", h.logoutUser)
+	r.HandleFunc("GET /me", h.getMe)
 }
 
-func (h *UserHandler) registerUser(w http.ResponseWriter, r *http.Request) {
+func (h *UserHandler) getMe(w http.ResponseWriter, r *http.Request) {
 
-	var body RegisterUserRequest
-	if err := json.ReadRequestBody(r, &body); err != nil {
-		json.WriteJSON(w, http.StatusBadRequest, json.Wrapper{"error": "Failed to process request body"}, nil)
-		return
-	}
-
-	token, err := h.svc.RegisterUser(
-		r.Context(),
-		body.FirstName,
-		body.LastName,
-		body.Email,
-		body.Password,
-		body.UserRole,
-	)
+	user, err := jwt.GetUserDataFromContext(r.Context())
 	if err != nil {
-		var fieldErr *apperr.FieldError
-		if errors.As(err, &fieldErr) {
-			json.WriteJSON(w, http.StatusUnprocessableEntity, json.Wrapper{"error": fieldErr}, nil)
-			return
-		}
-		json.WriteJSON(w, http.StatusInternalServerError, json.Wrapper{"error": "Failed to register user"}, nil)
+		json.WriteJSON(w, http.StatusBadRequest, "Could not get user", nil)
 		return
 	}
 
-	http.SetCookie(w, &http.Cookie{
-		Name:     "token",
-		Value:    token,
-		HttpOnly: true,
-		Secure:   false,
-		SameSite: http.SameSiteLaxMode,
-		MaxAge:   h.jwtAge,
-		Path:     "/",
-	})
-
-	json.WriteJSON(w, http.StatusOK, json.Wrapper{"token": token}, nil)
-}
-
-func (h *UserHandler) loginUser(w http.ResponseWriter, r *http.Request) {
-
-	var body LoginUserRequest
-	if err := json.ReadRequestBody(r, &body); err != nil {
-		json.WriteJSON(w, http.StatusBadRequest, json.Wrapper{"error": "Failed to process request body"}, nil)
-		return
-	}
-
-	token, err := h.svc.LoginUser(r.Context(), body.Email, body.Password)
-	if err != nil {
-		json.WriteJSON(w, http.StatusUnauthorized, json.Wrapper{"error": "Invalid username or password"}, nil)
-		return
-	}
-
-	http.SetCookie(w, &http.Cookie{
-		Name:     "token",
-		Value:    token,
-		HttpOnly: true,
-		Secure:   false,
-		SameSite: http.SameSiteLaxMode,
-		MaxAge:   h.jwtAge,
-		Path:     "/",
-	})
-
-	json.WriteJSON(w, http.StatusOK, json.Wrapper{"token": token}, nil)
-}
-
-// Cookie-only. For clearing the cookies on the
-// browser side. Always sends http.StatusNoContent (204)
-func (h *UserHandler) logoutUser(w http.ResponseWriter, r *http.Request) {
-
-	http.SetCookie(w, &http.Cookie{
-		Name:     "token",
-		HttpOnly: true,
-		Secure:   false,
-		SameSite: http.SameSiteLaxMode,
-		MaxAge:   -1,
-		Path:     "/",
-	})
-
-	json.WriteJSON(w, http.StatusNoContent, nil, nil)
+	fullUser, err := h.svc.Me(r.Context(), user)
+	json.WriteJSON(w, http.StatusOK, json.Wrapper{"user": fullUser}, nil)
 }
--- internal/core/users/service.go
+++ internal/core/users/service.go
@@ -2,182 +2,29 @@ package users
 
 import (
 	"context"
-	"errors"
-	"fmt"
-	"time"
 
 	store "github.com/acertainpoggerman/online-exam-system/internal/adapters/postgresql/sqlc"
 	"github.com/acertainpoggerman/online-exam-system/internal/apperr"
-	"github.com/acertainpoggerman/online-exam-system/internal/jwt"
-	"github.com/acertainpoggerman/online-exam-system/internal/passwords"
-	"github.com/google/uuid"
-	"github.com/jackc/pgx/v5/pgconn"
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 type UserService interface {
-	RegisterUser(ctx context.Context, firstName string, lastName string, email string, password string, role store.UserRole) (string, error)
-	LoginUser(ctx context.Context, email string, password string) (string, error)
-}
-
-type ExtUserService interface {
-	FindExtUserByID(ctx context.Context, userID uuid.UUID) (store.User, error)
+	Me(ctx context.Context, user store.User) (store.User, error)
 }
 
 type userService struct {
-	q             *store.Queries
-	pool          *pgxpool.Pool
-	jwtSecretKey  []byte
-	jwtExpiryTime time.Duration
+	q    *store.Queries
+	pool *pgxpool.Pool
 }
 
-func NewUserService(q *store.Queries, pool *pgxpool.Pool, jwtSecretKey []byte, jwtExpiryTime time.Duration) *userService {
-	return &userService{q, pool, jwtSecretKey, jwtExpiryTime}
+func NewUserService(q *store.Queries, pool *pgxpool.Pool) *userService {
+	return &userService{q, pool}
 }
 
-// -----------------------------------------------------------------------
-// --- Implementing ExtUserService Interface -----------------------------
-// -----------------------------------------------------------------------
-
-func (svc *userService) FindExtUserByID(ctx context.Context, userID uuid.UUID) (store.User, error) {
-
-	tx, err := svc.pool.Begin(ctx)
+func (svc *userService) Me(ctx context.Context, user store.User) (store.User, error) {
+	user, err := svc.q.FindUserByID(ctx, user.ID)
 	if err != nil {
-		return store.User{}, err
+		return store.User{}, apperr.ErrNotFound
 	}
-	defer tx.Rollback(ctx)
-	qtx := svc.q.WithTx(tx)
-
-	user, err := qtx.FindUserByID(ctx, userID)
-	if err != nil {
-		return store.User{}, err
-	}
-
-	return store.User{
-		FirstName: user.FirstName,
-		LastName:  user.LastName,
-		Email:     user.Email,
-	}, nil
-}
-
-// -----------------------------------------------------------------------
-// --- Implementing UserService Interface --------------------------------
-// -----------------------------------------------------------------------
-
-func (svc *userService) RegisterUser(
-	ctx context.Context,
-	firstName string,
-	lastName string,
-	email string,
-	password string,
-	role store.UserRole,
-) (string, error) {
-
-	// Hash Password
-
-	hash, err := passwords.GeneratePasswordHash(password)
-	if err != nil {
-		return "", err
-	}
-
-	tx, err := svc.pool.Begin(ctx)
-	if err != nil {
-		return "", err
-	}
-	defer tx.Rollback(ctx)
-	qtx := svc.q.WithTx(tx)
-
-	// Add Basic User Details
-
-	user, err := qtx.CreateUser(ctx, store.CreateUserParams{
-		FirstName:    firstName,
-		LastName:     lastName,
-		Email:        email,
-		PasswordHash: hash,
-		Role:         role,
-	})
-	if err != nil {
-		var pgErr *pgconn.PgError
-		if errors.As(err, &pgErr) && pgErr.Code == "23505" {
-			return "", &apperr.FieldError{
-				Field:   "email",
-				Message: "Email already in use",
-				Code:    "EMAIL_TAKEN",
-			}
-		}
-	}
-
-	// Add Specific "Role"
-
-	switch role {
-	case store.UserRoleExaminee:
-		_, err = qtx.CreateExaminee(ctx, user.ID)
-		if err != nil {
-			return "", err
-		}
-	case store.UserRoleExaminer:
-		_, err = qtx.CreateExaminer(ctx, user.ID)
-		if err != nil {
-			return "", err
-		}
-	default:
-		return "", fmt.Errorf("Requested user role: %v does not exist", role)
-	}
-
-	// Generate JWT for User
-
-	tokenString, err := jwt.CreateJWT(
-		user,
-		svc.jwtSecretKey,
-		svc.jwtExpiryTime,
-	)
-	if err != nil {
-		return "", err
-	}
-
-	return tokenString, tx.Commit(ctx)
-}
-
-func (svc *userService) LoginUser(ctx context.Context, email string, password string) (string, error) {
-
-	tx, err := svc.pool.Begin(ctx)
-	if err != nil {
-		return "", err
-	}
-	defer tx.Rollback(ctx)
-	qtx := svc.q.WithTx(tx)
-
-	// Find the user by email (emails are set to be unique in the pool)
-
-	user, err := qtx.FindUserByEmail(ctx, email)
-	if err != nil {
-		return "", err
-	}
-
-	// Check given password is correct
-
-	if !passwords.VerifyPassword(password, user.PasswordHash) {
-		return "", fmt.Errorf("Wrong username / password")
-	}
-
-	// Check role in database
-
-	switch user.Role {
-	case store.UserRoleExaminee:
-	case store.UserRoleExaminer:
-	default:
-	}
-
-	// Generate JWT for User
-
-	tokenString, err := jwt.CreateJWT(
-		user,
-		svc.jwtSecretKey,
-		svc.jwtExpiryTime,
-	)
-	if err != nil {
-		return "", err
-	}
-
-	return tokenString, tx.Commit(ctx)
+	return user, nil
 }
